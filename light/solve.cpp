7fab45d09d07be92283b77fc15a4891ce26eb758
Igortopchik229 <72626444+Igortopchik229@users.noreply.github.com>
2024-07-18 13:55:45 +0300

    Add files via upload

---
+++ light/solve.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+constexpr ll INF = 1e18;
+
+void solve() {
+    ll n, m; cin >> n >> m;
+    ll s, f; cin >> s >> f;
+    vector<vector<ll>>d(n, vector<ll>(n, INF));
+    for (int i = 0; i < m; i++) {
+        ll a, b; cin >> a >> b;
+        d[--a][--b] = 1;
+        d[b][a] = d[a][b];
+    }
+    for (int to = 0; to < n; to++) {
+        for (int u = 0; u < n; u++) {
+            for (int v = 0; v < n; v++) {
+                d[u][v] = min(d[u][v], d[u][to] + d[to][v]);
+            }
+        }
+    }
+    cout << d[--s][--f];
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    freopen("B.txt", "r", stdin);
+    solve();
+    return 0;
+}
